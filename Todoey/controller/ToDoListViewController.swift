5e362583c20a572c6a482c837c8559271eb3ed7b
Orkhan Bayramli <bayramliorxan1998@gmail.com>
2019-10-09 15:25:57 +0200

    Adding ToDo Model class

--- Todoey/controller/ToDoListViewController.swift
+++ Todoey/controller/ToDoListViewController.swift
@@ -6,11 +6,23 @@
 //  Copyright © 2019 Orkhan Bayramli. All rights reserved.
 //
 
+
+/*
+ 
+ **DISCLAIMER**
+ 
+ There is a bug in the App so that once the ToDo item is clicked, this click is also shown in another cell.
+ It is because of the cell that we use. We make tableView use the dequeueReusableCell() methode when
+ constructing the cells. In this case, the cell that we touch is used again when it is gone off the
+ screen. As we are assigning the checkmark property to cell, this checkmark property also stays in the
+ cell which is used again.
+ 
+ */
 import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    var itemArray = ["Salam", "Necesen", "Yaxshiyam"]
+    var itemArray = [ToDoItem]()
     
     let defaults = UserDefaults.standard
     
@@ -19,10 +31,22 @@ class ToDoListViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let item = ToDoItem()
+        item.title = "First"
+        itemArray.append(item)
+        
+        let item2 = ToDoItem()
+        item2.title = "Second"
+        itemArray.append(item2)
+        
+        let item3 = ToDoItem()
+        item3.title = "Third"
+        itemArray.append(item3)
+        
         // Getting the stored data from UserDefaults
-        if let items = UserDefaults.standard.array(forKey: "ToDoListArray") as? [String] {
-            itemArray = items
-        }
+//        if let items = UserDefaults.standard.array(forKey: "ToDoListArray") as? [String] {
+//            itemArray = items
+//        }
     }
     
     //MARK - Tableview Datasource Methods
@@ -32,8 +56,18 @@ class ToDoListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        let item = itemArray[indexPath.row]
+        
+        // **Ternary operator**
+        //
+        // If item.isDone is true the assign .checkmark to the cell.accessoryType,
+        // otherwise assign .none
+        cell.accessoryType = item.isDone == true ? .checkmark : .none
+
         return cell
     }
     
@@ -41,14 +75,11 @@ class ToDoListViewController: UITableViewController {
     //MARK - Tableview Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(itemArray[indexPath.row])
         
-        // Conditional statement for adding and removing checkmark to the ToDoItemCell
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        // Little trick for setting the isDone property
+        itemArray[indexPath.row].isDone = !itemArray[indexPath.row].isDone
+    
+        tableView.reloadData()
         
         // When clicking, it deselects the row in an animated way
         tableView.deselectRow(at: indexPath, animated: true)
@@ -59,21 +90,29 @@ class ToDoListViewController: UITableViewController {
     // Creating UIAlert for adding new item
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
+        // Reference to the textfield since it is inside of the closure
         var textField = UITextField()
         
         let alert = UIAlertController(title: "Add New ToDo Item", message: "", preferredStyle: .alert)
         
+        // Adding the ToDo item to the UserDefaults
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            self.itemArray.append(textField.text!)
+            
+            let newItem = ToDoItem()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
             self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             self.tableView.reloadData()
         }
         
+        // Adding textfield for user to enter the ToDo
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create New Item"
             textField = alertTextField
         }
         
+        // Finalizing to show the UIAlert
         alert.addAction(action)
         present(alert, animated: true, completion:   nil)
     }
---
+++ Todoey/model/ToDoItem.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoCell.swift
+//  Todoey
+//
+//  Created by Orkhan Bayramli on 10/9/19.
+//  Copyright © 2019 Orkhan Bayramli. All rights reserved.
+//
+
+import Foundation
+
+class ToDoItem {
+    
+    var title : String = ""
+    var isDone : Bool = false
+    
+    
+}
